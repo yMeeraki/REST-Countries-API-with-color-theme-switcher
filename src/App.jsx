4fef4366375c22c636eae7ffe7bf3a62f5186615
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-23 01:22:05 +0530

    feat: Country Card component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import CountryCard from "./components/CountryCard/CountryCard.jsx";
 import Header from "./components/Header/Header.jsx";
 
+import country_data from "../country_data.js";
+
 function App() {
+  const allCountries = country_data.map((country) => {
+    return <CountryCard key={country.name} flag={country.flags.png} countryName={country.name} region={country.region}  capital={country.capital}/>;
+  });
+
   return (
     <>
       <Header />
-      
+      <main className="p-10 bg-secondary/20 gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {allCountries}
+      </main>
     </>
   );
 }
---
+++ src/components/CountryCard/CountryCard.jsx
@@ -0,0 +1,33 @@
+function CountryCard(props) {
+  return (
+    <>
+      <section className="rounded-lg cursor-pointer p-5 grid grid-rows-1 gap-3 bg-primary/30  backdrop-blur-md shadow-2xl hover:scale-105 transition delay-150 duration-300 ease-in-out">
+        <div className="rounded-lg flex items-center">
+          <img
+            className="w-full rounded-lg"
+            src={props.flag}
+            alt={props.countryName}
+          />
+        </div>
+        <div className="bg-primary p-2 rounded">
+          <h2 className="text-xl font-extrabold text-accent">
+            {props.countryName}
+          </h2>
+        </div>
+
+        <div className="grid grid-cols-2 gap-5">
+          <div className="items-center">
+            <span className="text-sm font-bold">Capital: </span>
+            <p className="text-lg">{!props.capital ? "N/A" : props.capital}</p>
+          </div>
+          <div className=" items-center">
+            <span className="text-sm font-bold">Region: </span>
+            <p className="text-lg">{props.region}</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default CountryCard;
