1603d0769e8e04e9994050e19687a6a6756fb9e6
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-23 01:32:32 +0530

    feat: created CountryDetailCard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import CountryCard from "./components/CountryCard/CountryCard.jsx";
 import Header from "./components/Header/Header.jsx";
 
 import country_data from "../country_data.js";
+import CountryDetailCard from "./components/CountryCard/CountryDetailCard.jsx";
 
 function App() {
   const allCountries = country_data.map((country) => {
-    return <CountryCard key={country.name} flag={country.flags.png} countryName={country.name} region={country.region}  capital={country.capital}/>;
+    return (
+      <CountryCard
+        key={country.name}
+        flag={country.flags.png}
+        countryName={country.name}
+        region={country.region}
+        capital={country.capital}
+      />
+    );
   });
 
   return (
     <>
       <Header />
-      <main className="p-10 bg-secondary/20 gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {allCountries}
+      <main className="bg-secondary/20">
+        {/* <div className="p-10  gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {allCountries}
+        </div> */}
+        <CountryDetailCard/>
       </main>
     </>
   );
---
+++ src/components/CountryCard/CountryDetailCard.jsx
@@ -0,0 +1,9 @@
+function CountryDetailCard(props) {
+  return (
+    <>
+      <article className="p-10 "></article>
+    </>
+  );
+}
+
+export default CountryDetailCard;
