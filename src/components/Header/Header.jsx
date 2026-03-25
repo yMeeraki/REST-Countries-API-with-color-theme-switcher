ed1caf200a24f705ae36e5858cad7b6896d8a771
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-25 17:30:07 +0530

    feat: add dark mode toggle in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
-import { SunMoon } from "lucide-react";
+import { Sun, Moon } from "lucide-react";
 import SearchCountry from "../Search-And-Filter/SearchCountry.jsx";
 import Filter from "../Search-And-Filter/Filter.jsx";
+import React from "react";
 
 function Header() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+  function changeMode() {
+    setIsDarkMode((prev) => !prev);
+  }
+
   return (
     <>
       <header className="bg-secondary/10 shadow-xl shadow-primary/50 h-auto">
@@ -10,10 +16,21 @@ function Header() {
           <h1 className="text-primary text-2xl font-bold hover:underline hover:cursor-pointer tracking-wide  ">
             What in the World?
           </h1>
-          <span className="flex items-center gap-2 p-2 text-primary hover:scale-110 hover:cursor-pointer   transition delay-150 duration-300 ease-in-out">
-            <SunMoon />
-            <p>Dark Mode</p>
-          </span>
+          <button onClick={changeMode}>
+            <span className="flex items-center gap-2 p-2 text-primary hover:scale-110 hover:cursor-pointer   transition delay-150 duration-300 ease-in-out">
+              {isDarkMode ? (
+                <>
+                  <Moon />
+                  <p>Dark Mode</p>
+                </>
+              ) : (
+                <>
+                  <Sun />
+                  <p>Light Mode</p>
+                </>
+              )}
+            </span>
+          </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 p-5 gap-5">
           <SearchCountry />
